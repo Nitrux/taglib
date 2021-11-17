636f994b3e75c928cc147c6c502aa9cd1aaa6ca8
Uri Herrera <hello@uriherrera.com>
2021-11-17 17:54:47 -0600

    push pkg

--- travis-build.sh
+++ travis-build.sh
@@ -1,88 +1,22 @@
-#!/bin/bash
+#! /bin/sh
 
-set -x
+apt -qq update
+apt -qq -yy install equivs curl git
 
-### Install Build Tools #1
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
+git_commit=$(git rev-parse --short HEAD)
 
-DEBIAN_FRONTEND=noninteractive apt -qq update
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
-	appstream \
-	automake \
-	autotools-dev \
-	build-essential \
-	checkinstall \
-	cmake \
-	curl \
-	devscripts \
-	equivs \
-	extra-cmake-modules \
-	gettext \
-	git \
-	gnupg2 \
-	lintian \
-	wget
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: taglib-compat" \
+	"Version: 0.0.1-$git_commit" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Conflicts: libtag1v5, libtag1v5-vanilla, libtag1-dev" \
+	"Provides: libtag1v5, libtag1v5-vanilla, libtag1-dev" \
+	"Architecture: amd64" \
+	"Description: Transitional package for taglib."
 
-### Install Package Build Dependencies #2
-
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
-	zlib1g-dev \
-	libboost-dev \
-	libicu-dev \
-	pkg-kde-tools \
-	doxygen \
-	graphviz \
-	gsfonts-x11
-
-### Clone repo.
-
-git clone --single-branch --branch v1.12 https://github.com/taglib/taglib.git
-
-rm -rf taglib/{AUTHORS,COPYING.LGPL,COPYING.MPL,INSTALL.md,NEWS,README.md}
-
-### Compile Source
-
-mkdir -p taglib/build && cd taglib/build
-
-cmake \
-	-DCMAKE_INSTALL_PREFIX=/usr \
-	-DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_USE_RELATIVE_PATHS=ON \
-	-DWITH_ASF=ON \
-	-DWITH_MP4=ON \
-	-DLIB_INSTALL_DIR:PATH=/usr/lib/x86_64-linux-gnu \
-	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..
-
-make
-
-### Run checkinstall and Build Debian Package
-### DO NOT USE debuild, screw it
-
->> description-pak printf "%s\n" \
-	'audio meta-data library.' \
-	'' \
-	'TagLib is a library for reading and editing audio meta data, commonly know as tags.' \
-	'' \
-	'This package contains the development headers and runtime package for programs for.' \
-	'the TagLib Audio Meta-Data Library.' \
-	'' \
-	''
-
-checkinstall -D -y \
-	--install=no \
-	--fstrans=yes \
-	--pkgname=taglib \
-	--pkgversion=1.12.0 \
-	--pkgarch=amd64 \
-	--pkgrelease="1" \
-	--pkglicense=LGPL-1 \
-	--pkggroup=lib \
-	--pkgsource=taglib \
-	--pakdir=../.. \
-	--maintainer=uri_herrera@nxos.org \
-	--provides=taglib \
-	--requires=libc6,libgcc-s1,libicu66,libstdc++6,zlib1g \
-	--nodoc \
-	--strip=no \
-	--stripso=yes \
-	--reset-uids=yes \
-	--deldesc=yes
+equivs-build configuration
